09f598e88f1d01fbbf466d61fa12b12d25cee94d
Mohammed Elgaily <mbajily@gmail.com>
2024-09-30 17:52:53 +0000

    Iterate through object

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -2,4 +2,3 @@ export default function iterateThroughObject(reportWithIterator) {
   // Convert the iterator into an array using the spread operator and join the array with ' | '
   return [...reportWithIterator].join(' | ');
 }
-
