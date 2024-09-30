ff3157818939292e252fb02dbda80c55657c66e0
Mohammed Elgaily <mbajily@gmail.com>
2024-09-30 16:22:02 +0000

    Add permissions

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,17 +1,15 @@
-// I prefer const when I can. But sometimes let is okay
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
+  const task = 'I prefer const when I can.';
+  return task;
+}
 
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
