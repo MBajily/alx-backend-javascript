b90e074c6ff921cfbf7e2b21e97f61c603b13417
Mohammed Elgaily <mbajily@gmail.com>
2024-10-02 19:19:32 +0000

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,19 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  // Create an array of promises
+  const promises = [
+    signUpUser(firstName, lastName), // Call signUpUser
+    uploadPhoto(fileName), // Call uploadPhoto
+  ];
+
+  // Use Promise.allSettled to wait for both promises to settle
+  const results = await Promise.allSettled(promises);
+
+  // Map the results to the desired structure
+  return results.map((result) => ({
+    status: result.status, // Status of the promise
+    value: result.status === 'fulfilled' ? result.value : result.reason, // Value or error
+  }));
 }
