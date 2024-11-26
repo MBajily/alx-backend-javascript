996bdb64008d8d0a208e5a108b6d78995f0ed779
Mohammed Elgaily <mbajily@gmail.com>
2024-11-26 20:43:23 +0000

    0x05. NodeJS Basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,11 @@
 // 1-stdin.js
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
-
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
 
-  if (chunk !== null) {
-    process.stdout.write(`Your name is: ${chunk.trim()}\n`);
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
